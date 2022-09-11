f745c9add57d2e6b0bafb7ab3ae8e57ae47a3890
Leah <76621057+Leahh147@users.noreply.github.com>
2022-09-11 19:07:59 +0800

    Update nms.cu
    head files changed

--- nms.cu
+++ nms.cu
@@ -2,7 +2,8 @@
 #include <ATen/ATen.h>
 #include <ATen/cuda/CUDAContext.h>
 
-#include <THC/THC.h>
+#include <THC/THCAtomics.cuh>
+#include <THC/THCDeviceUtils.cuh>
 #include <THC/THCDeviceUtils.cuh>
 
 #include <vector>
